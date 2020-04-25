c6d32b54ae7166ae1a55b6816918dfc1b8effe3a
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-26 01:36:35 +0200

    add frontend sample and backend mock /pdf route

--- app/views/frontend.py
+++ app/views/frontend.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+import uuid
+from flask import Blueprint, render_template, current_app, request
 
 frontend = Blueprint("frontend", __name__)
 
@@ -7,3 +8,11 @@ frontend = Blueprint("frontend", __name__)
 @frontend.route('/index')
 def index():
     return render_template("index.html")
+
+
+@frontend.route('/pdf', methods=["POST"])
+def generate_pdf():
+    uu_id = uuid.uuid4()
+    current_app.logger.debug(uu_id)
+    current_app.logger.debug(request.form)
+    return "PDF will be downloaded"
