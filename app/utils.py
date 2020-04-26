901d72a65f4afa5ee2ef30a74608cbd508d685fe
labaffa <dellarovere.lante@gmail.com>
2020-04-26 16:17:05 +0200

    Add fields_to_pdf function

--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,7 @@ import pdfkit
 import jinja2
 import os
 
+SEARCH_PATH = "./"
 TEMPLATE_PATH = "templates/modulo.html"
 
 
@@ -20,3 +21,21 @@ def html_to_pdf(path_or_string, pdf_path):
         pdfkit.from_file(path_or_string, pdf_path)
     else:
         pdfkit.from_string(path_or_string, pdf_path)
+
+
+def fields_to_pdf(
+        fields, out_path,
+        template_relpath=TEMPLATE_PATH,
+        template_searchpath=SEARCH_PATH):
+    """
+    Params
+    ------
+    fields (dict): fields to fill template with
+    out_path: pdf destination path
+    template_searchpath: search root folder of the filesystem
+    template_relpath: path of modulo template relative to template_searchpath
+
+    """
+    template = get_template(path=template_relpath, searchpath=SEARCH_PATH)
+    compiled = compile_template(template, **fields)
+    html_to_pdf(compiled, out_path)
