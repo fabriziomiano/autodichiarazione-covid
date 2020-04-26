b3eeaeb4d19a57c5dc93cb4a02ecda9c9c174f2c
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-26 18:49:34 +0200

    improve security

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
+import os
+
 from flask import Flask
+from flask_wtf import CSRFProtect
+
 from .views.frontend import frontend
 
 
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = os.urandom(32)
     app.register_blueprint(frontend)
+    CSRFProtect(app)
     return app
