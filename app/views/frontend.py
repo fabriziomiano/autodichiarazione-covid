4ccc1dca61d2db26bf4ac882e3d6418976c6d28a
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-26 18:51:06 +0200

    add REGIONS

--- app/views/frontend.py
+++ app/views/frontend.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Blueprint, render_template, current_app, request
+from app.config import REGIONS
 
 frontend = Blueprint("frontend", __name__)
 
@@ -7,7 +8,7 @@ frontend = Blueprint("frontend", __name__)
 @frontend.route('/')
 @frontend.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", regions=REGIONS)
 
 
 @frontend.route('/pdf', methods=["POST"])
@@ -15,4 +16,6 @@ def generate_pdf():
     uu_id = uuid.uuid4()
     current_app.logger.debug(uu_id)
     current_app.logger.debug(request.form)
+    fields = list(request.form.keys())
+    current_app.logger.debug("Got {} fields".format(len(fields)))
     return "PDF will be downloaded"
