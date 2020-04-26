5ea4ca017b050459deb89be4bebcab7201db4227
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-27 01:02:43 +0200

    add download-and-remove functionality

--- app/views/frontend.py
+++ app/views/frontend.py
@@ -1,8 +1,12 @@
+import os
 import uuid
-from flask import Blueprint, render_template, current_app, request
-from app.config import REGIONS
-from app.utils import fields_to_pdf
 
+from flask import (
+    Blueprint, render_template, current_app, request, redirect, url_for
+)
+
+from app.config import REGIONS, DEFAULT_FILENAME
+from app.utils import fields_to_pdf
 
 frontend = Blueprint("frontend", __name__)
 
@@ -15,9 +19,38 @@ def index():
 
 @frontend.route('/pdf', methods=["POST"])
 def generate_pdf():
-    uu_id = uuid.uuid4()
-    current_app.logger.debug(uu_id)
-    current_app.logger.debug(request.form)
-    current_app.logger.debug("Got {} fields".format(len(list(request.form.keys()))))
-    fields_to_pdf(request.form, "./{}.pdf".format(uu_id))
-    return "PDF will be downloaded"
+    """
+    Get a client-side validated form, create the PDF, and redirect to
+    download_and_remove route
+    """
+    uu_id = uuid.uuid4().hex
+    current_app.logger.debug(uu_id, request.form)
+    pdf_filename = "{}.pdf".format(uu_id)
+    out_pdf_path = os.path.join(
+        current_app.root_path,
+        current_app.config['UPLOAD_FOLDER'],
+        pdf_filename
+    )
+    fields_to_pdf(request.form, out_pdf_path)
+    return redirect(
+        url_for('frontend.download_and_remove', filename=pdf_filename)
+    )
+
+
+@frontend.route('/download_and_remove/<filename>')
+def download_and_remove(filename):
+    """
+    Serve file with filename in UPLOAD_FOLDER before deleting it
+    :param filename: str <uuid>.pdf
+    """
+    path = os.path.join(
+        current_app.root_path, current_app.config['UPLOAD_FOLDER'], filename)
+
+    def generate():
+        with open(path, "rb") as f:
+            yield from f
+        os.remove(path)
+
+    r = current_app.response_class(generate(), mimetype='application/pdf')
+    r.headers.set('Content-Disposition', 'attachment', filename=DEFAULT_FILENAME)
+    return r
