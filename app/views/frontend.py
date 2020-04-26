af1b5fb7a1b401351f3a162fd99686a6e75dd12e
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-26 19:41:40 +0200

    add call to fields_to_pdf()

--- app/views/frontend.py
+++ app/views/frontend.py
@@ -1,6 +1,9 @@
 import uuid
+import os
 from flask import Blueprint, render_template, current_app, request
 from app.config import REGIONS
+from app.utils import fields_to_pdf
+
 
 frontend = Blueprint("frontend", __name__)
 
@@ -16,6 +19,6 @@ def generate_pdf():
     uu_id = uuid.uuid4()
     current_app.logger.debug(uu_id)
     current_app.logger.debug(request.form)
-    fields = list(request.form.keys())
-    current_app.logger.debug("Got {} fields".format(len(fields)))
+    current_app.logger.debug("Got {} fields".format(len(list(request.form.keys()))))
+    fields_to_pdf(request.form, "./{}.pdf".format(uu_id))
     return "PDF will be downloaded"
