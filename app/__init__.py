deef872d9a4497398311cad8b05b6ed2992366a5
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-27 01:03:13 +0200

    add folder to serve static files

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,9 @@ from .views.frontend import frontend
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static')
     app.config["SECRET_KEY"] = os.urandom(32)
+    app.config["UPLOAD_FOLDER"] = os.path.join(app.static_folder, 'pdf')
     app.register_blueprint(frontend)
     CSRFProtect(app)
     return app
