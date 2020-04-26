572d38b543b136de8eee45e7d2a65c2672e7e524
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-26 18:49:05 +0200

    add config

---
+++ app/config.py
@@ -0,0 +1,7 @@
+REGIONS = [
+    'Abruzzo', 'Basilicata', 'Calabria', 'Campania', 'Emilia-Romagna',
+    'Friuli Venezia Giulia', 'Lazio', 'Liguria', 'Lombardia',
+    'Marche', 'Molise', 'P.A. Bolzano', 'P.A. Trento', 'Piemonte',
+    'Puglia', 'Sardegna', 'Sicilia', 'Toscana', 'Umbria',
+    "Valle d'Aosta", 'Veneto'
+]
