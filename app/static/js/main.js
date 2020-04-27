fc9431e1dd940cdb084bd9d6a9d7270ef8141e34
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-27 16:43:57 +0200

    add main.js

---
+++ app/static/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+    $('#birthdate').datetimepicker({
+        format: 'L',
+        locale: 'it'
+    });
+});
+
+$(function () {
+    $('#id_release_date').datetimepicker({
+        format: 'L',
+        locale: 'it'
+    })
+});
