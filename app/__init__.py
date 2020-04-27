11974fc7843e0b4c65dcc7094aaa4bba8520c7b8
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-27 02:16:38 +0200

    deactivate CSRF Protection as it's a pain in the arse to handle with docker, gunicorn and wsgi

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-from flask_wtf import CSRFProtect
+# from flask_wtf import CSRFProtect
 
 from .views.frontend import frontend
 
@@ -11,5 +11,5 @@ def create_app():
     app.config["SECRET_KEY"] = os.urandom(32)
     app.config["UPLOAD_FOLDER"] = os.path.join(app.static_folder, 'pdf')
     app.register_blueprint(frontend)
-    CSRFProtect(app)
+    # CSRFProtect(app)
     return app
