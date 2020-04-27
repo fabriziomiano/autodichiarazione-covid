0eb76bfeb53956682b69bd7b27c0de05f29c56fc
labaffa <dellarovere.lante@gmail.com>
2020-04-27 08:35:47 +0200

    Allow use of wkhtmltopdf options in utils functions

--- app/utils.py
+++ app/utils.py
@@ -17,14 +17,30 @@ def compile_template(template, **kw):
     return template.render(**kw)
 
 
-def html_to_pdf(path_or_string, pdf_path):
+def html_to_pdf(path_or_string, pdf_path, options=None):
+    """
+    Params
+    ------
+    path_or_string (str) -- if path read from file else from given string
+    pdf_path (str) -- where to save produced PDF
+    options (dict or None) -- wkhtmltopdf options, for example
+        options = {
+            'page-size':'A4',
+            'dpi':400,
+            'margin-top':'0.5cm',
+            'margin-bottom':'0.5cm',
+            'margin-left':'1cm',
+            'margin-right':'1cm'
+        }
+
+    """
     if os.path.isfile(path_or_string):
-        pdfkit.from_file(path_or_string, pdf_path)
+        pdfkit.from_file(path_or_string, pdf_path, options=options)
     else:
-        pdfkit.from_string(path_or_string, pdf_path)
+        pdfkit.from_string(path_or_string, pdf_path, options=options)
 
 
-def fields_to_pdf(fields, out_path):
+def fields_to_pdf(fields, out_path, pdf_options=None):
     """
     Write PDF file to out_path
     :param fields: dict
@@ -32,4 +48,4 @@ def fields_to_pdf(fields, out_path):
     :return: None
     """
     compiled_template = render_template(TEMPLATE_PATH, **fields)
-    html_to_pdf(compiled_template, out_path)
+    html_to_pdf(compiled_template, out_path, options=pdf_options)
