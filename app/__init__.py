a9efc4f48ac15de79f4b81bdffe5b6246d8d95b4
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-27 16:45:07 +0200

    make it slimmer

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 import os
 
 from flask import Flask
-# from flask_wtf import CSRFProtect
-
 from .views.frontend import frontend
 
 
@@ -11,5 +9,4 @@ def create_app():
     app.config["SECRET_KEY"] = os.urandom(32)
     app.config["UPLOAD_FOLDER"] = os.path.join(app.static_folder, 'pdf')
     app.register_blueprint(frontend)
-    # CSRFProtect(app)
     return app
--- app/views/frontend.py
+++ app/views/frontend.py
@@ -1,12 +1,6 @@
-import os
-import uuid
+from flask import Blueprint, render_template, request
 
-from flask import (
-    Blueprint, render_template, current_app, request, redirect, url_for
-)
-
-from app.config import REGIONS, DEFAULT_FILENAME
-from app.utils import fields_to_pdf
+from app.config import REGIONS
 
 frontend = Blueprint("frontend", __name__)
 
@@ -17,40 +11,6 @@ def index():
     return render_template("index.html", regions=REGIONS)
 
 
-@frontend.route('/pdf', methods=["POST"])
-def generate_pdf():
-    """
-    Get a client-side validated form, create the PDF, and redirect to
-    download_and_remove route
-    """
-    uu_id = uuid.uuid4().hex
-    current_app.logger.debug(uu_id, request.form)
-    pdf_filename = "{}.pdf".format(uu_id)
-    out_pdf_path = os.path.join(
-        current_app.root_path,
-        current_app.config['UPLOAD_FOLDER'],
-        pdf_filename
-    )
-    fields_to_pdf(request.form, out_pdf_path)
-    return redirect(
-        url_for('frontend.download_and_remove', filename=pdf_filename)
-    )
-
-
-@frontend.route('/download_and_remove/<filename>')
-def download_and_remove(filename):
-    """
-    Serve file with filename in UPLOAD_FOLDER before deleting it
-    :param filename: str <uuid>.pdf
-    """
-    path = os.path.join(
-        current_app.root_path, current_app.config['UPLOAD_FOLDER'], filename)
-
-    def generate():
-        with open(path, "rb") as f:
-            yield from f
-        os.remove(path)
-
-    r = current_app.response_class(generate(), mimetype='application/pdf')
-    r.headers.set('Content-Disposition', 'attachment', filename=DEFAULT_FILENAME)
-    return r
+@frontend.route('/renderform', methods=["POST"])
+def showform():
+    return render_template("bsform_template.html", **request.form)
