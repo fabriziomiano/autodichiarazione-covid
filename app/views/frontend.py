8036d96f97b5a00768b069a822504e25bb5a5ee7
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-28 02:38:25 +0200

    add timestamp to force client-side js reload

--- app/views/frontend.py
+++ app/views/frontend.py
@@ -1,3 +1,4 @@
+import time
 from flask import Blueprint, render_template, request
 
 from app.config import REGIONS
@@ -8,9 +9,17 @@ frontend = Blueprint("frontend", __name__)
 @frontend.route('/')
 @frontend.route('/index')
 def index():
-    return render_template("index.html", regions=REGIONS)
+    return render_template(
+        "index.html",
+        regions=REGIONS,
+        ts=time.time()
+    )
 
 
 @frontend.route('/renderform', methods=["POST"])
 def showform():
-    return render_template("form_template.html", **request.form)
+    return render_template(
+        "form_template.html",
+        **request.form,
+        ts=time.time()
+    )
