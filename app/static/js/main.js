32eb8f8c9948bac107d08d873dfb61313ef0fb9a
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-28 02:12:26 +0200

    add function to copy residence values to domicile if checkbox is checked else delete fields

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -11,3 +11,23 @@ $(function () {
         locale: 'it'
     })
 });
+
+$(function () {
+    $("#checkboxResidenza").on('change', function () {
+        if ($(this).prop("checked") === true) {
+            $("#validazioneDomicilio").prop("disabled", true)
+            $("#validazioneIndirizzoDomicilio").prop("disabled", true)
+            $("#validazioneProvinciaDomicilio").prop("disabled", true)
+            $('#validazioneDomicilio').val($('#validazioneResidenza').val());
+            $('#validazioneIndirizzoDomicilio').val($('#validazioneIndirizzoResidenza').val());
+            $('#validazioneProvinciaDomicilio').val($('#validazioneProvinciaResidenza').val());
+        } else {
+            $("#validazioneDomicilio").prop("disabled", false)
+            $("#validazioneIndirizzoDomicilio").prop("disabled", false)
+            $("#validazioneProvinciaDomicilio").prop("disabled", false)
+            $('#validazioneDomicilio').val("");
+            $('#validazioneIndirizzoDomicilio').val("");
+            $('#validazioneProvinciaDomicilio').val("");
+        }
+    })
+})
