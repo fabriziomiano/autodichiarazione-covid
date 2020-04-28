9aa07747c76f47f00b7b9513a36176112281d6a4
Fabrizio Miano <fabriziomiano@gmail.com>
2020-04-28 16:22:57 +0200

    add app configuration parameters

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,15 @@ import os
 
 from flask import Flask
 from .views.frontend import frontend
+from .config import PDF_TEMPLATE_FILENAME
 
 
 def create_app():
     app = Flask(__name__, static_folder='static')
     app.config["SECRET_KEY"] = os.urandom(32)
     app.config["UPLOAD_FOLDER"] = os.path.join(app.static_folder, 'pdf')
+    app.config["PDF_TEMPLATE_PATH"] = os.path.join(
+        app.config["UPLOAD_FOLDER"], PDF_TEMPLATE_FILENAME
+    )
     app.register_blueprint(frontend)
     return app
